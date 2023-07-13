a44214929ad11c40937ddfb110bf445d7ae70ee0
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-07-12 23:30:40 -0600

    made information  required and made it so it displays what type of card the user is entering

--- front-end/src/Components/CardInfo.js
+++ front-end/src/Components/CardInfo.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, {useState}from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CardInformationForm() {
+
+    const [cardNumber, setCardNumber] = useState('');
+  const [cardType, setCardType] = useState('');
+
+
+  //this is found from the internet to not have to figure out the logic
+  const handleCardNumberChange = (event) => {
+    const cardNumber = event.target.value.replace(/\s/g, '');
+    let newCardType = '';
+
+    if (cardNumber.startsWith('4')) {
+      newCardType = 'Visa';
+    } else if (cardNumber.startsWith('5')) {
+      newCardType = 'Mastercard';
+    } else if (cardNumber.startsWith('3')) {
+      if (cardNumber.charAt(1) === '4' || cardNumber.charAt(1) === '7') {
+        newCardType = 'American Express';
+      }
+    } else if (cardNumber.startsWith('6')) {
+      if (cardNumber.slice(0, 4) === '6011' || (cardNumber.charAt(1) === '4' && cardNumber.length >= 16)) {
+        newCardType = 'Discover';
+      }
+    }
+
+    setCardType(newCardType);
+    setCardNumber(cardNumber); // Update the cardNumber state
+  };
+
+
   return (
     <form>
       <div className="mb-3">
-        <label htmlFor="cardNumber" className="form-label">Card Number</label>
-        <input type="text" className="form-control" id="cardNumber" />
+      <label htmlFor="cardNumber" className="form-label">Card Number</label>
+        <input type="text" maxLength={16} className="form-control" id="cardNumber" value={cardNumber} onChange={handleCardNumberChange} required/>
+        {cardType && 
+        <p>Card Type: {cardType}</p>
+        }
       </div>
       <div className="row">
         <div className="col-md-6 mb-3">
           <label htmlFor="expiration" className="form-label">Expiration Date</label>
-          <input type="text" className="form-control" id="expiration" />
+          <input type="text" maxLength={10} pattern='^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$' className="form-control" id="expiration" placeholder='DD/MM/YYYY' required />
         </div>
         <div className="col-md-6 mb-3">
           <label htmlFor="cvv" className="form-label">CVV</label>
-          <input type="text" className="form-control" id="cvv" />
+          <input type="text" pattern="\d{3,4}" maxLength={4}  className="form-control" id="cvv" placeholder='CVV' required/>
         </div>
       </div>
       <div className="mb-3">
         <label htmlFor="cardHolder" className="form-label">Cardholder Name</label>
-        <input type="text" className="form-control" id="cardHolder" />
+        <input type="text" className="form-control" id="cardHolder" placeholder='Name' required/>
       </div>
       <button type="submit" className="btn-primary">Submit</button>
     </form>
--- front-end/src/Components/CheckoutForm.js
+++ front-end/src/Components/CheckoutForm.js
@@ -14,46 +14,42 @@ function CheckoutForm() {
     }
 
 
-
-
-
-
-
   return (
+    <>
     <form className='checkoutform'>
       <div className="mb-3">
         <label htmlFor="firstName" className="form-label">First Name</label>
-        <input type="text" className="form-control" id="firstName" />
+        <input type="text" className="form-control" id="firstName" placeholder='First' required/>
       </div>
       <div className="mb-3">
         <label htmlFor="lastName" className="form-label">Last Name</label>
-        <input type="text" className="form-control" id="lastName" />
+        <input type="text" className="form-control" id="lastName" placeholder='Last Name' required />
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email</label>
-        <input type="email" className="form-control" id="email" />
+        <input type="email" className="form-control" id="email" placeholder='Email' required/>
       </div>
       <div className="mb-3">
         <label htmlFor="address" className="form-label">Address</label>
-        <input type="text" className="form-control" id="address" />
+        <input type="text" className="form-control" id="address" placeholder='Address' required/>
       </div>
       <div className="mb-3">
         <label htmlFor="city" className="form-label">City</label>
-        <input type="text" className="form-control" id="city" />
+        <input type="text" className="form-control" id="city"  placeholder='City' required/>
       </div>
       <div className="mb-3">
         <label htmlFor="zip" className="form-label">Zip Code</label>
-        <input type="text" className="form-control" id="zip" />
+        <input type="text" pattern="\d{5}" maxLength={5} className="form-control" id="zip" placeholder='Zipcode' required/>
       </div> 
       {!showCard && (
             <button type="submit" onClick={handlesubmit} className="btn-primary">Submit</button>
 
       )}
-      {showCard && <CardInfo /> }
+      
     </form>
-
+{showCard && <CardInfo /> }
    
-
+</>
   );
 }
 
