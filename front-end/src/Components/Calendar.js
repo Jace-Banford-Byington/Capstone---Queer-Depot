aefd06d904cbcd660a39b8fbc41541afedf16566
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-08-02 19:41:41 -0600

    Added more events

--- front-end/src/Components/Calendar.js
+++ front-end/src/Components/Calendar.js
@@ -7,47 +7,36 @@ const Calendar = () => {
 
 
     const holidays = [
-        
-        "Martian Luther King Jr. Birthday - 1/17",
-        "Chinese New Year - 1/21 thur 2/20",
-        "Lincoln's Birthday - 2/12",
-        "Valentines Day - 2/14",
-        "Presidents Day 2/21",
-        "Washington's Birthday - 2/21",
+       
+      "Chinese New Year - 1/21 thur 2/20",
+
         "Mardi Gras - 47 Days before Easter 3/23 through 4/25",
         "Ash Wednesday - Day after Mardi Gras",
         "Purim - 14th of Adar", //Need to look into what means
-        "Saint Patrick's Day - 3/17",
-        
         "Ramadan - ninth month of the Islamic calendar ",
+       
         "Palm Sunday - sunday before Easter",
         "Good Friday - Friday before Easter", 
+       
         "Passover - 15thd day of Hebrew Month  Nisan first month in Hebrew calander", 
         "Easter - first sunday after the 14th ",
         "Orthodox Easter - first sunday full moon after Passover",
 
         "Eid al-Fitr - end of Ramadon Fasting first 3 days of Shawwāl (10th month of islamic month",
-        "Cinco de Mayo - 5/5",
-        "VE-Day - 5/8",
-        "Mother's Day - second Sunday in May",
         "Ascention Day - 39 days after Easter",
-        "Memorial Day - last monday in may",
-        "Pride Month - 6/1 throuh 6/31",
+       
         "Shavuot - 50 days after passover ",
         "Pentecost = 50 days affter resurrection",
-        "Father's Day - third sinday in June",
        
         "Eid al-Adha - 10 days after the sighting of a new crescent moon. lasts 3 days",
         "Muharram - first month of islamic New Year begins after first signting of the new moon on final day of islamic calendar",
-        "Labor Day - first monday of september",
         "Rosh Hashanah - 163 days after passover",
         "Yom Kippur - 10th day of Tishrei 9 days after first day of Rosh Hashanah",
         "Nawlid al-Nabi - 12th day of Rabi 1  Always on the same day in Islamic Calendar",
         "Sukkot 15th - 21s of Tishrei",
         "Shemini Atzeret  - 22nd day of Tishrei (in Land of Isreal) 22nd and 23rd (everwhere else)",
         "Diwali - lasts  5 day / determined by position of the moon ",
-        "Election Day - first monday in November ",
-        "National Day of Mouring - fourth thursday in November",
+       
         "First Sunday of Advent - nearest sunday to 9/30",
         "Hanukkah - 25th day of Kislev",
     ]
@@ -64,9 +53,22 @@ const Calendar = () => {
               {title: "Epiphany", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=6"},
 
               {title: "GroundHog's Day", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=2"},
+              {title: "Martin Luther King Jr. Birthday", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=17"},
+              {title: "Lincoln's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=17"},
 
               {title: "April Fools", rrule: "FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=1"},
-             
+              {title: "Valentine's Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=14"},
+              {title: "Presidents Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21"},
+              {title: "Washington's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21"},
+              {title: "Saint Partick's Day", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=17"},
+              {title: "Memorial Day", rrule: "FREQ=YEARLY;BYMONTH=5MO;BYMONTHDAY=-1"},
+              {title: "Father's Day", rrule: "FREQ=YEARLY;BYMONTH=6;BYDAY=3SU"},
+              {title: "Mother's Day", rrule: "FREQ=YEARLY;BYMONTH=5;BYDAY=2SU"},
+              {title: "Labor Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYDAY=1MO"},
+              {title: "Election Day", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=1MO"},
+              {title: "National Day of Mourning", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=4TH"},
+
+
               {title: "Spring Equinox", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=21"},
               {title: "Autumn Equinox", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=23"},
               {title: "Summer Solstice", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=21"},
@@ -81,21 +83,10 @@ const Calendar = () => {
               {title: "All Hallows", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1"},
               {title: "Samhain", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1"},
 
-
-
-
-
-
-
-
-
-
-
-
-
               { title: "May Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1" },
               { title: "Beltane", rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1" },
-              { title: "Cinco de Mayo", date:"2023-05-05" },
+              { title: "Cinco de Mayo",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=5" },
+              { title: "V-E Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=8" },
 
 
               { title: "Flag Day",rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=14"  },
