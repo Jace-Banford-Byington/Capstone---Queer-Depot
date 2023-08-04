701f19d61826e9820e0524dff528a3e142ab38f7
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-08-03 19:52:21 -0600

    Google Maps is implemented and shows where you are (blue) and where you want to go (red)
    The design of the map container needs to be worked on

--- front-end/src/Components/Map.js
+++ front-end/src/Components/Map.js
@@ -1,37 +1,46 @@
 //API KEy: AIzaSyDEN8GuGjy8uGkQdDiHHI2Zi-bJw9MLLWU
 import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker  } from '@react-google-maps/api';
-
+ const apiKey = 'AIzaSyDEN8GuGjy8uGkQdDiHHI2Zi-bJw9MLLWU';
+    const destination = { lat: 40.673110, lng: -111.865000 }; 
+    const containerStyle = {
+        width: '100%',
+        height: '400px'
+    };
 //LAt 40.673110
 //Long -111.865000
 const Map = () => {
-    const apiKey = 'AIzaSyDEN8GuGjy8uGkQdDiHHI2Zi-bJw9MLLWU';
-    const destination = { lat: 40.673110, lng: -111.865000 };
+    const [currentPosition, setCurrentPosition] = useState(null);
+  
+    useEffect(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentPosition({ lat: latitude, lng: longitude });
+        });
+      }
+    }, []);
 
-
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-};
-const [currentPosition, setCurrentPosition] = useState(null);
-
-useEffect(() => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setCurrentPosition({ lat: latitude, lng: longitude });
-    });
-  }
-}, []);
-
-return (
-  <LoadScript googleMapsApiKey={apiKey}>
-    <GoogleMap mapContainerStyle={containerStyle} center={currentPosition} zoom={10}>
-      {currentPosition && <Marker position={currentPosition} title="Current Location" />}
-      <Marker position={destination} title="Destination" />
-    </GoogleMap>
-  </LoadScript>
-);
-};
-
-export default Map;
+    const currentLocationIcon = {
+        url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if needed
+      };
+    
+      const destinationIcon = {
+        url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if needed
+      };
+  
+    return (
+        <LoadScript googleMapsApiKey={apiKey}>
+        {currentPosition && (
+          <GoogleMap mapContainerStyle={containerStyle} center={currentPosition} zoom={10}>
+            <Marker position={currentPosition} icon={currentLocationIcon} />
+            <Marker position={destination} icon={destinationIcon} />
+          </GoogleMap>
+        )}
+      </LoadScript>
+    );
+  };
+  
+  export default Map;
