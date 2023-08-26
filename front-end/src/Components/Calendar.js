048cf3b3759d340e6d294ae0ab365f908e4d0fc3
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2023-08-25 20:34:21 -0600

    Can psuedo add an event

--- front-end/src/Components/Calendar.js
+++ front-end/src/Components/Calendar.js
@@ -3,12 +3,10 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import rrulePlugin from "@fullcalendar/rrule";
 import { RRule } from "rrule";
-
-
+import Popup from './Popup';
 
 const Calendar = () => {
   const [auth, setAuth] = useState(false);
-  const [newEvent, setNewEvent] = useState({ title: '', start: '', end: '' });
   const [selectedDate, setDate] = useState(null)
   const [popupOpen, setPopupOpen] = useState(false);
 
@@ -20,35 +18,18 @@ const Calendar = () => {
     }
   }, [])
 
-
-  const handleEventChange = (eventField, value) => {
-    setNewEvent((prevEvent) => ({ ...prevEvent, [eventField]: value }));
-  };
-
-  const handleDayClicked = (args) => {
+  const handleDayClicked = (args) =>   {
     const clickedDate = new Date(args.date);
-    console.log("Clicked Date: ", clickedDate)
-    clickedDate.setUTCHours(0, 0, 0, 0); // Convert to UTC midnight
-    console.log("After being shifted: ", clickedDate)
-
-    const eventsOnClickedDate = customEvents.filter((event) => {
-      console.log("Starting Filltering process")
-      console.log("The events on clicked date",eventsOnClickedDate)
-      if (event.rrule) {
-        const rule = RRule.fromString(event.rrule);
-        const occurrences = rule.between(clickedDate, clickedDate, true);
-        return occurrences.length > 0;
-      }
-      return false;
-    });
+    setDate(clickedDate);
+    setPopupOpen(true);
+};
   
-    if (eventsOnClickedDate.length === 0) {
-      setDate(clickedDate);
-      setPopupOpen(true);
-    }
-  };
-  
-
+  const handleNewEvent = (eventData) => {
+     // Add logic to save the eventData as a new event
+     console.log('New event data:', eventData);
+     setPopupOpen(false); // Close the popup after saving
+  }
+ 
 
     const handlePopUpClose = () => {
       setPopupOpen(false);
@@ -56,81 +37,81 @@ const Calendar = () => {
 
 
     const customEvents = [
-      {title: "Epiphany", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=6"},
+      {title: "Epiphany", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=6;BYHOUR=0;BYMINUTE=0"},
 
-      {title: "GroundHog's Day", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=2"},
-      {title: "Martin Luther King Jr. Birthday", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=17"},
-      {title: "Lincoln's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=17"},
+      {title: "GroundHog's Day", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=2;BYHOUR=0;BYMINUTE=0"},
+      {title: "Martin Luther King Jr. Birthday", rrule: "FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=17;BYHOUR=0;BYMINUTE=0"},
+      {title: "Lincoln's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=17;BYHOUR=0;BYMINUTE=0"},
 
-      {title: "April Fools", rrule: "FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=1"},
-      {title: "Valentine's Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=14"},
-      {title: "Presidents Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21"},
-      {title: "Washington's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21"},
-      {title: "Saint Partick's Day", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=17"},
-      {title: "Memorial Day", rrule: "FREQ=YEARLY;BYMONTH=5MO;BYMONTHDAY=-1"},
-      {title: "Father's Day", rrule: "FREQ=YEARLY;BYMONTH=6;BYDAY=3SU"},
-      {title: "Mother's Day", rrule: "FREQ=YEARLY;BYMONTH=5;BYDAY=2SU"},
-      {title: "Labor Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYDAY=1MO"},
-      {title: "Election Day", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=1MO"},
-      {title: "National Day of Mourning", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=4TH"},
+      {title: "April Fools", rrule: "FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0"},
+      {title: "Valentine's Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=14;BYHOUR=0;BYMINUTE=0"},
+      {title: "Presidents Day", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Washington's Birthday", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Saint Partick's Day", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=17;BYHOUR=0;BYMINUTE=0"},
+      {title: "Memorial Day", rrule: "FREQ=YEARLY;BYMONTH=5MO;BYMONTHDAY=-1;BYHOUR=0;BYMINUTE=0"},
+      {title: "Father's Day", rrule: "FREQ=YEARLY;BYMONTH=6;BYDAY=3SU;BYHOUR=0;BYMINUTE=0"},
+      {title: "Mother's Day", rrule: "FREQ=YEARLY;BYMONTH=5;BYDAY=2SU;BYHOUR=0;BYMINUTE=0"},
+      {title: "Labor Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYDAY=1MO;BYHOUR=0;BYMINUTE=0"},
+      {title: "Election Day", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=1MO;BYHOUR=0;BYMINUTE=0"},
+      {title: "National Day of Mourning", rrule: "FREQ=YEARLY;BYMONTH=11;BYDAY=4TH;BYHOUR=0;BYMINUTE=0"},
 
 
-      {title: "Spring Equinox", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=21"},
-      {title: "Autumn Equinox", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=23"},
-      {title: "Summer Solstice", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=21"},
-      {title: "Winter Equinox", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=22"},
+      {title: "Spring Equinox", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Autumn Equinox", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=23;BYHOUR=0;BYMINUTE=0"},
+      {title: "Summer Solstice", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Winter Equinox", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=22;BYHOUR=0;BYMINUTE=0"},
       {title: "Yule", 
-        rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=21",
+        rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0",
         duration: { days: 12 }
       },
-      {title: "Imbolic", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=1"},
-      {title: "Ostara", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=21"},
-      {title: "Litha", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=21"},
-      {title: "Midsummer", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=24"},
-      {title: "Lughnasdh", rrule: "FREQ=YEARLY;BYMONTH=8;BYMONTHDAY=1"},
-      {title: "Madon", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=23"},
-      {title: "All Hallows", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1"},
-      {title: "Samhain", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1"},
+      {title: "Imbolic", rrule: "FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0"},
+      {title: "Ostara", rrule: "FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Litha", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=21;BYHOUR=0;BYMINUTE=0"},
+      {title: "Midsummer", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=24;BYHOUR=0;BYMINUTE=0"},
+      {title: "Lughnasdh", rrule: "FREQ=YEARLY;BYMONTH=8;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0"},
+      {title: "Madon", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=23;BYHOUR=0;BYMINUTE=0"},
+      {title: "All Hallows", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0"},
+      {title: "Samhain", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0"},
 
-      { title: "May Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1" },
-      { title: "Beltane", rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1" },
-      { title: "Cinco de Mayo",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=5" },
-      { title: "V-E Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=8" },
+      { title: "May Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0" },
+      { title: "Beltane", rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0" },
+      { title: "Cinco de Mayo",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=5;BYHOUR=0;BYMINUTE=0" },
+      { title: "V-E Day",rrule: "FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=8;BYHOUR=0;BYMINUTE=0" },
 
 
-      { title: "Flag Day",rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=14"  },
-      { title: "Juneteenth", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=19" },
+      { title: "Flag Day",rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=14;BYHOUR=0;BYMINUTE=0"  },
+      { title: "Juneteenth", rrule: "FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=19;BYHOUR=0;BYMINUTE=0" },
 
-      { title: "Forth of July",rrule: "FREQ=YEARLY;BYMONTH=7;BYMONTHDAY=4" },
-      { title: "Pioneer Day", rrule: "FREQ=YEARLY;BYMONTH=7;BYMONTHDAY=24" },
+      { title: "Forth of July",rrule: "FREQ=YEARLY;BYMONTH=7;BYMONTHDAY=4;BYHOUR=0;BYMINUTE=0" },
+      { title: "Pioneer Day", rrule: "FREQ=YEARLY;BYMONTH=7;BYMONTHDAY=24;BYHOUR=0;BYMINUTE=0" },
      
 
 
-      { title: "Indigenous Peoples' Day", rrule:"FREQ=YEARLY;BYDAY=2MO;BYMONTH=10"},
-      { title: "Halloween",rrule: "FREQ=YEARLY;BYMONTH=10;BYMONTHDAY=31" },
+      { title: "Indigenous Peoples' Day", rrule:"FREQ=YEARLY;BYDAY=2MO;BYMONTH=10;BYHOUR=0;BYMINUTE=0"},
+      { title: "Halloween",rrule: "FREQ=YEARLY;BYMONTH=10;BYMONTHDAY=31;BYHOUR=0;BYMINUTE=0" },
 
 
-      { title: "Veterans Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=11" },
-      { title: "All Saints Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=1" },
+      { title: "Veterans Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=11;BYHOUR=0;BYMINUTE=0" },
+      { title: "All Saints Day", rrule: "FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0" },
 
       { title: "Day of the Dead",
-        rrule:"FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1",
+        rrule:"FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0",
         duration: {days: 2}
       },
 
-      { title: "Trans Day of Remembrance", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=20" },
+      { title: "Trans Day of Remembrance", rrule: "FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=20;BYHOUR=0;BYMINUTE=0" },
 
 
       { title: "Kwanzaa",
       
-      rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=26",
+      rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=26;BYHOUR=0;BYMINUTE=0",
       duration: {days: 7}
     },
-      { title: "Chrismas", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=25" },
-      { title: "Christmas Eve", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=24" },
+      { title: "Chrismas", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=25;BYHOUR=0;BYMINUTE=0" },
+      { title: "Christmas Eve", rrule: "FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=24;BYHOUR=0;BYMINUTE=0" },
       { title: "New Years Eve", date: "2023-12-31" },
 
-      {title: "New Years", rrule:"FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=1" }
+      {title: "New Years", rrule:"FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=1;BYHOUR=0;BYMINUTE=0" }
 
     ];
 
@@ -175,6 +156,9 @@ const Calendar = () => {
             }}
 					/> 
 				</div>
+      {popupOpen && (
+        <Popup isOpen={popupOpen} onClose={handlePopUpClose} onSave={handleNewEvent} />            
+        )}
    </div>
   )
 }
--- front-end/src/Components/Popup.js
+++ front-end/src/Components/Popup.js
@@ -1,6 +1,89 @@
+import React, { useState } from 'react'
+
+const Popup = ({isOpen, onClose, onSave}) => {
+  const [name,setName] = useState('')
+  const [startTime, setStartTime] = useState('')
+  const [endTime, setEndTime] = useState('')
+  const [descripton, setDescription] = useState('')
+
 //need 
 //  name 
 //  staet time 
 //  end time
 
 //Optional for a discription
+
+  const handleSubmit = () => {
+   
+  }
+  
+  const handleSave = () => {
+    const newEvent = {
+        Name: name,
+        StartTime: startTime,
+        EndTime: endTime,
+        Description: descripton
+    };
+
+    onSave(newEvent)  //saves the event 
+
+    //Now that event has been saved clear the entered data 
+    setName('');
+    setStartTime('');
+    setEndTime('');
+    setDescription('');
+    onClose();
+   
+  }
+  
+  const handleCancel = () => {
+    // Clear the input fields and close the popup
+    setName('');
+    setStartTime('');
+    setEndTime('');
+    setDescription('');
+    onClose();
+  };
+  
+    return (
+    <div className='popup'>
+        <h1>Add a New Event</h1>
+            <label>Name:</label>
+            <input 
+                type='text'
+                placeholder='Event Name'
+                value={name}
+                onChange={(e) => setName(e.target.value) }
+            />
+
+            <label>Start Time:</label>
+                <input 
+                    type='time'
+                    placeholder='Start Time'
+                    value={startTime}
+                    onChange={(e) => setStartTime(e.target.value)}
+                />
+
+            <label>End Time:</label>
+                <input 
+                    type='time'
+                    placeholder='End Time'
+                    value={endTime}
+                    onChange={(e) => setEndTime(e.target.value)}
+                />
+
+            <label>Description</label>
+                <input
+                    type='text'
+                    placeholder='Description of Event'
+                    value={descripton}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            <button onClick={handleSave}>Save Event</button>
+            <button onClick={handleCancel}>Cancel</button>
+
+    </div>
+  )
+}
+
+export default Popup
